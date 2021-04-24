4e259dd53be242d59982b078656c46ead38eff49
pabbate00 <81483600+pabbate00@users.noreply.github.com>
2021-04-24 10:56:11 +0200

    Add files via upload

---
+++ Hello world-3.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[14]:
+
+
+###This notebook will be mainly used for the capstone project
+
+
+# In[15]:
+
+
+import pandas as pd
+
+
+# In[16]:
+
+
+import numpy as np
+
+
+# In[17]:
+
+
+print('Hello Capstone Project Course!')
+
+
+# In[ ]:
+
+
+
+
